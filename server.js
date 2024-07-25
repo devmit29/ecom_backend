e59e88c5d9db4b651cbbfd0ee3cda2468fa789b4
Dev Mittal <mittal.dev2002@gmail.com>
2024-07-25 12:40:58 +0530

    allowing local host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://ecom-frontend-4x7f.onrender.com', // frontend URL
+    origin: ['http://localhost:3000', 'https://ecom-frontend-4x7f.onrender.com'], // frontend URL
     credentials: true // allow cookies to be sent with requests
 }))
 app.use(fileUpload({
